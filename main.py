50b17f424133f63e8843a72b14e0ca5022408d87
cs-apple <dimitri.dietz93@gmail.com>
2025-07-31 21:06:59 -0400

    Success load tilesheet, open console window!

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-import tcod
+from __future__ import annotations
+import tcod.context
+import tcod.tileset 
 
 
-def main():
-    print("Hello World!")
-
+def main() -> None:
+    """Load a tileset and open a window using it, this window will immediatly close"""
+    tileset = tcod.tileset.load_tilesheet(
+        "data/Alloy_curses_12x12.png", columns=16, rows=16, charmap=tcod.tileset.CHARMAP_CP437
+    )
+    tcod.tileset.procedural_block_elements(tileset=tileset)
+    with tcod.context.new(tileset=tileset) as context:
+        pass
 
 if __name__ == "__main__":
     main()
